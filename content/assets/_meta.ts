4daa7b9e85346ea42afb4aac8ff72055c754bad1
odudename <odude.com@gmail.com>
2026-02-01 08:34:43 +0545

    docs

--- content/assets/_meta.ts
+++ content/assets/_meta.ts
@@ -1,4 +1,7 @@
 export default {
-    index: 'Overview',
-    inventory: 'Inventory'
+    index: 'About Assets',
+    management: 'Asset Management',
+    owner: 'Owner Dashboard',
+    transactions: 'Transaction History',
+    types: 'Asset Types'
 }
