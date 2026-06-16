a3e0dcdd5c9360fbe58c90156cf1f2e89b4b72be
odudename <odude.com@gmail.com>
2026-06-16 20:43:44 +0545

    hide assets and point from menu

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,9 +1,18 @@
 export default {
   index: 'Introduction',
   names: 'Odude Names',
-  marketplace: 'Marketplace',
-  points: 'Points',
-  assets: 'Assets',
+  marketplace: {
+    title: 'Marketplace',
+    display: 'hidden', // Hides this and all nested pages from the sidebar/navigation
+  },
+  credits: {
+    title: 'Credits',
+    display: 'normal', // Hides this and all nested pages from the sidebar/navigation
+  },
+  assets: {
+    title: 'Assets',
+    display: 'hidden', // Hides this and all nested pages from the sidebar/navigation
+  },
   bookmarks: 'Bookmarks',
   api: 'API References',
 }
